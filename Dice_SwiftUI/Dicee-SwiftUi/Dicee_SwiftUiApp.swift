a5ffa1c0b5065c57c281d527fa30df493cd7eec3
Иван <102241612+Ivan-Biriukov@users.noreply.github.com>
2023-03-20 22:54:58 +0300

    Add files via upload

---
+++ Dice_SwiftUI/Dicee-SwiftUi/Dicee_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUiApp.swift
+//  Dicee-SwiftUi
+//
+//  Created by иван Бирюков on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
